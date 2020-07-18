2f9b2871cd46ced2b0afe95f83d7a66f37b5b202
Ilya Pushkarev <ilyapushkarev10@gmail.com>
2020-07-18 13:11:16 +0300

    Updates

---
+++ precache-manifest.a554a3e59e8187f781d4cfb1dc786dc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810112cd5854171cff63ece60f301bb6",
+    "url": "/currency-conversion/index.html"
+  },
+  {
+    "revision": "be2730daa05f4829aa25",
+    "url": "/currency-conversion/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "6fdc53852f5de2351d44",
+    "url": "/currency-conversion/static/css/main.7eab1e5f.chunk.css"
+  },
+  {
+    "revision": "be2730daa05f4829aa25",
+    "url": "/currency-conversion/static/js/2.9c0fa864.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/currency-conversion/static/js/2.9c0fa864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fdc53852f5de2351d44",
+    "url": "/currency-conversion/static/js/main.fe168b65.chunk.js"
+  },
+  {
+    "revision": "0f9cb666be59c9fcc28b",
+    "url": "/currency-conversion/static/js/runtime-main.a837aa66.js"
+  }
+]);
